7e2d545318ff422364d91041caccaf83caa06b11
SpencerCzarnezki <czarnez5@gmail.com>
2024-02-20 17:32:34 -0600

    removed example material

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,12 +10,9 @@ import frc.robot.commands.TeleopSwerveCommand;
 import frc.robot.commands.LimelightDriveCom;
 import frc.robot.subsystems.LimelightSub;
 import frc.robot.subsystems.SwerveSubSystem;
-import frc.robot.commands.Autos;
 import frc.robot.commands.ClimbCom;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.GroundIntakeCom;
 import frc.robot.subsystems.ClimbSub;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.GroundIntakeSub;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -31,7 +28,6 @@ import edu.wpi.first.math.util.Units;
 public class RobotContainer {
   private final LimelightSub m_LimelightSub = new LimelightSub();
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final GroundIntakeSub m_GroundIntakeSub = new GroundIntakeSub();
   private final ClimbSub m_ClimbSub = new ClimbSub(); // Hello! I wasn't looking when someone was typing
   // Replace with CommandPS4Controller or CommandJoystick if needed
@@ -60,13 +56,6 @@ public class RobotContainer {
     // m_driverController.x().whileTrue(new LimelightDriveCom(swerveSubSystem,
     // m_LimelightSub));
 
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
-    // pressed,
-    // cancelling on release.
     m_driverController.b().whileTrue(new GroundIntakeCom(m_GroundIntakeSub, 0));
     m_driverController.x().whileTrue(new ClimbCom(m_ClimbSub, 0, 0));
     m_driverController.a().whileTrue(new ClimbCom(m_ClimbSub, 0, 0));
